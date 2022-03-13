8f5cb4d214e6ae966945f9a4422b4c0580b1528b
埃博拉酱 <vhtmfu@outlook.com>
2022-03-13 19:30:36 +0800

    v1.1.0

--- KMS激活/MainWindow.xaml.vb
+++ KMS激活/MainWindow.xaml.vb
@@ -1,9 +1,13 @@
 ﻿Imports System.ComponentModel
 Imports System.IO
+
 Class MainWindow
 	Private 密钥列表 As String()
-	ReadOnly 设置目录 As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+	ReadOnly 设置目录 As String = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
 	ReadOnly 设置路径 As String = Path.Combine(设置目录, "KMS激活设置.bin")
+	ReadOnly 启动信息 As New ProcessStartInfo With {.CreateNoWindow = True, .FileName = "powershell.exe", .UseShellExecute = False, .WindowStyle = ProcessWindowStyle.Hidden, .RedirectStandardOutput = True, .RedirectStandardError = True}
+	ReadOnly 目录选择对话框 As New Microsoft.Win32.OpenFileDialog With {.Filter = "OSPP.VBS|OSPP.VBS"}
+
 	Sub 读入设置()
 		Dim 读入器 As BinaryReader
 		Try
@@ -16,6 +20,7 @@ Class MainWindow
 			Width = 读入器.ReadUInt16
 			Height = 读入器.ReadUInt16
 			KMS服务器地址.Text = 读入器.ReadString
+			OSPP位置.Text = 读入器.ReadString
 			Dim 版本选项 As SByte = 读入器.ReadSByte
 			If 版本选项 < 0 Then
 				版本或密钥.Text = 读入器.ReadString
@@ -29,46 +34,28 @@ Class MainWindow
 		End Try
 		读入器.Close()
 	End Sub
+
 	Sub 写出设置()
 		Call Directory.CreateDirectory(设置目录)
 		Using 写出器 As New BinaryWriter(File.OpenWrite(设置路径))
 			写出器.Write(CUShort(Width))
 			写出器.Write(CUShort(Height))
 			写出器.Write(KMS服务器地址.Text)
+			写出器.Write(OSPP位置.Text)
 			写出器.Write(CSByte(版本或密钥.SelectedIndex))
 			写出器.Write(版本或密钥.Text)
 		End Using
 	End Sub
-	Sub New()
-
-		' This call is required by the designer.
-		InitializeComponent()
-
-		' Add any initialization after the InitializeComponent() call.
-		Dim 文件读取器 As New BinaryReader(File.OpenRead(Path.Combine(Path.GetDirectoryName(Process.GetCurrentProcess.MainModule.FileName), "密钥库.bin")))
-		Dim 选项个数 As Byte = 文件读取器.ReadByte
-		Dim 操作系统版本(选项个数 - 1) As String
-		For a As Byte = 0 To 选项个数 - 1
-			操作系统版本(a) = 文件读取器.ReadString
-		Next
-		版本或密钥.ItemsSource = 操作系统版本
-		ReDim 密钥列表(选项个数 - 1)
-		For a As Byte = 0 To 选项个数 - 1
-			密钥列表(a) = 文件读取器.ReadString
-		Next
-		文件读取器.Close()
-	End Sub
 
 	Private Sub 激活Windows_Click(sender As Object, e As RoutedEventArgs) Handles 激活Windows.Click
-		Static 启动信息 As New ProcessStartInfo With {.CreateNoWindow = True, .FileName = "powershell.exe", .UseShellExecute = False, .WindowStyle = ProcessWindowStyle.Hidden, .RedirectStandardOutput = True, .RedirectStandardError = True}
 		Dim 服务器地址 As String = KMS服务器地址.Text
 		Dim 密钥 = If(版本或密钥.SelectedIndex < 0, 版本或密钥.Text, 密钥列表(版本或密钥.SelectedIndex))
 		'防注入检查
-		If 服务器地址.Contains("'") OrElse 服务器地址.Contains(vbCrLf) Then
+		If 服务器地址.Contains("'"c) OrElse 服务器地址.Contains(vbCrLf) Then
 			Windows激活结果.Text = "服务器地址含有非法字符"
 			Return
 		End If
-		If 密钥.Contains("'") OrElse 密钥.Contains(vbCrLf) Then
+		If 密钥.Contains("'"c) OrElse 密钥.Contains(vbCrLf) Then
 			Windows激活结果.Text = "密钥含有非法字符"
 			Return
 		End If
@@ -78,15 +65,18 @@ Class MainWindow
 	End Sub
 
 	Private Sub 激活Office_Click(sender As Object, e As RoutedEventArgs) Handles 激活Office.Click
-		Static 启动信息 As New ProcessStartInfo With {.CreateNoWindow = True, .FileName = "powershell.exe", .UseShellExecute = False, .WindowStyle = ProcessWindowStyle.Hidden, .RedirectStandardOutput = True, .RedirectStandardError = True}
 		Dim 服务器地址 As String = KMS服务器地址.Text
 		'防注入检查
-		If 服务器地址.Contains("'") OrElse 服务器地址.Contains(vbCrLf) Then
+		If 服务器地址.Contains("'"c) OrElse 服务器地址.Contains(vbCrLf) Then
 			Office激活结果.Text = "服务器地址含有非法字符"
 			Return
 		End If
-		启动信息.Arguments = "Start-Process powershell -ArgumentList @('cd ''C:\Program Files\Microsoft Office\Office16'';cscript OSPP.VBS /sethst:" & 服务器地址 & ";cscript OSPP.VBS /act') -Verb Runas"
-		Office激活结果.Text = ""
+		If File.Exists(Path.Combine(OSPP位置.Text, "OSPP.VBS")) Then
+			启动信息.Arguments = "Start-Process powershell -ArgumentList @('cd ''" & OSPP位置.Text & "'';cscript OSPP.VBS /sethst:" & 服务器地址 & ";cscript OSPP.VBS /act;pause') -Verb Runas"
+			Office激活结果.Text = ""
+		Else
+			Office激活结果.Text = "没有找到OSPP，请检查你的Office版本和安装位置"
+		End If
 		Call Process.Start(启动信息)
 	End Sub
 
@@ -94,7 +84,31 @@ Class MainWindow
 		写出设置()
 	End Sub
 
-	Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+	Private Async Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+		Dim 文本列表 = Await File.ReadAllLinesAsync(Path.Combine(Path.GetDirectoryName(Environment.ProcessPath), "密钥库.txt"))
+		Dim 选项上限 As Byte = 文本列表.Length - 1
+		Dim 操作系统版本(选项上限) As String
+		ReDim 密钥列表(选项上限)
+		Dim 两项 As String()
+		For a = 0 To 选项上限
+			两项 = 文本列表(a).Split(vbTab)
+			操作系统版本(a) = 两项(0)
+			密钥列表(a) = 两项(1)
+		Next
+		版本或密钥.ItemsSource = 操作系统版本
+		Dim 当前版本 = (New Win32APINET.WinBrand).BrandingFormatString("%WINDOWS_LONG%")
+		For a = 0 To 选项上限
+			If 操作系统版本(a) = 当前版本 Then
+				版本或密钥.SelectedIndex = a
+				Exit For
+			End If
+		Next
 		读入设置()
+	End Sub
+
+	Private Sub 设置OSPP位置_Click(sender As Object, e As RoutedEventArgs) Handles 设置OSPP位置.Click
+		If 目录选择对话框.ShowDialog Then
+			OSPP位置.Text = Path.GetDirectoryName(目录选择对话框.FileName)
+		End If
 	End Sub
 End Class
